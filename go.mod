7f2cb582c346a43a1ca4a3f518e1650bae7e927b
Neil O'Connor <neil@pop-os.neiloconnor.com>
2025-12-01 17:31:54 +0000

    initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/neilo40/adventofcode2025
+
+go 1.24
---
+++ helper/helper.go
@@ -0,0 +1,39 @@
+package helper
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const session = ""
+
+func DownloadInput() {
+	fInfo, _ := os.Stat("input.txt")
+	if fInfo != nil {
+		return
+	}
+
+	p, _ := os.Getwd()
+	d := filepath.Base(p)
+	dNum, _ := strconv.Atoi(strings.TrimPrefix(d, "day"))
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("https://adventofcode.com/2025/day/%d/input", dNum), nil)
+	cookie := new(http.Cookie)
+	cookie.Name, cookie.Value = "session", session
+	req.AddCookie(cookie)
+	req.Header.Set("User-Agent", "neilo40's golang client")
+	c := &http.Client{Timeout: 60 * time.Second}
+	r, err := c.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+	bodyBytes, _ := io.ReadAll(r.Body)
+	os.WriteFile("input.txt", bodyBytes, 0644)
+}
